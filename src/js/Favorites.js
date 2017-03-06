aff1f622fc8128de7d492a9939c3ce7650a17b6e
Michael Resnick <biglowresnick@rcn.com>
2017-03-06 17:10:27 -0500

    Rework Favorites list, by default list is dortable, edit now only deletes and alters name

--- src/js/Favorites.js
+++ src/js/Favorites.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// TODO: Reset editing mode when leaving this page
 /* exported favorites */
 const Favorites = (() => {
   const _FAVORITES = 'favorites';
@@ -45,14 +46,37 @@ const Favorites = (() => {
 
     _favorites.forEach((favorite) => {
       displayList.append(
-        `<li class="swipeout" data-name="${favorite.name}">
-          <div class="swipeout-content item-content">
+/* XXX
+        `<li>
+          <div class="item-content">
             <div class="item-media">
-              <a href="#" class="favorite-delete"><i class="icon ion-android-remove-circle"></i></a>
+              <a href="#" class="favorite-delete">
+                <i class="icon ion-android-remove-circle"></i>
+              </a>
             </div>
-            <div class="item-inner">
-              <div class="item-title">${favorite.name} (${favorite.decoratedText})</div>
+            <a href="#">
+              <div class="item-inner">
+                <div class="item-title">${favorite.name}</div>
+                <div class=", fter">${favorite.decoratedText}</div>
+              </div>
+            </a>
+          </div>
+          <div class="sortable-handler"></div>
+        </li>`
+*/
+        `<li class="swipeout" data-name="${favorite.name}">
+          <div class="item-content swipeout-content">
+            <div class="item-media">
+              <a href="#" class="favorite-delete">
+                <i class="icon ion-android-remove-circle"></i>
+              </a>
             </div>
+            <a href='#'>
+              <div class="item-inner">
+                <div class="item-title">${favorite.name}</div>
+                <div class=", fter">${favorite.decoratedText}</div>
+              </div>
+            </a>
           </div>
           <div class="sortable-handler"></div>
           <div class="swipeout-actions-right">
@@ -62,31 +86,67 @@ const Favorites = (() => {
       );
     });
 
+/* XXX
+          <div class=", fter">
+            <a href="#" class="edit-item editing"><i class="icon ion-chevron-right"></i></a>
+          </div>
+*/
+    // TODO: Create "_enterEdit()/_exitEdit()"
+
     $('#favorites .sortable li').on('sortable:sort', (event) => {
       _move(event.detail.startIndex, event.detail.newIndex);
     });
 
     $('#favorites .favorite-delete').click(event => boadApp.swipeoutOpen($(event.target).closest('li')));
 
-    $('#favorites li.swipeout').on('swipeout:open', () => { boadApp.sortableClose('#favorites .sortable'); });
-    $('#favorites li.swipeout').on('swipeout:close', () => { boadApp.sortableOpen('#favorites .sortable'); });
+    // XXX
+    // function _onSwipeoutOpen(event) {
+    //   boadApp.sortableClose('#favorites .sortable');
+    //   $()
+    // }
+    $('#favorites li.swipeout').on('swipeout:open', () => {
+      boadApp.sortableClose('#favorites .sortable');
+      // XXX $('#favorites .list-block ., fter a').each((index, element) => { $(element).prop('disabled', true); });
+    });
+    $('#favorites li.swipeout').on('swipeout:close', () => {
+      boadApp.sortableOpen('#favorites .sortable');
+      // XXX $('#favorites .list-block ., fter a').each((element) => { $(element).prop('disabled', false); });
+    });
     $('#favorites li.swipeout').on('swipeout:delete', (event) => {
       _delete($(event.target).data('name'));
       boadApp.sortableOpen('#favorites .sortable');
+      // XXX $('#favorites .list-block ., fter a').each((element) => { $(element).prop('disabled', false); });
     });
+
+    // XXX
+    // $('#favorites .list-block li ., fter a').click(() => {
+    //   if ($('#favorites .list-block li.swipeout.swipeout-opened').length === 0) {
+    //     console.log('click');
+    //   }
+    //   else {
+    //     console.log('(silence - force close)');
+    //   }
+    // });
   }
 
   function _getInstance() { return _instance; }
 
   $('#favorites .navbar-inner .left .main').click(() => {
     $('#favorites').addClass('editing');
-    boadApp.sortableOpen('#favorites .sortable');
+    $('#favorites .list-block .item-content > a:not(.item-link)').addClass('item-link');
+      //   boadApp.sortableOpen('#favorites .sortable');
   });
 
   $('#favorites .navbar-inner .left .editing').click(() => {
     $('#favorites').removeClass('editing');
-    boadApp.sortableClose('#favorites .sortable');
+    $('#favorites .list-block .item-content > a.item-link').removeClass('item-link');
+  //   boadApp.sortableClose('#favorites .sortable');
   });
+
+  // TEMP fix to clear old format
+  if (localStorage.getItem(_FAVORITES) === JSON.stringify([null, null, null, null, null, null, null, null, null, null])) {
+    localStorage.removeItem(_FAVORITES);
+  }
 
   _favorites = _getLocalStorage(_FAVORITES, []);
 
