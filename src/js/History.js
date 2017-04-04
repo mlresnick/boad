26a41b0aa842b6afd312ca533b8e906f0c219c89
Michael Resnick <biglowresnick@rcn.com>
2017-04-04 17:49:42 -0400

    Remove duplicate '=>' in history list entries

--- src/js/History.js
+++ src/js/History.js
@@ -1,5 +1,3 @@
-// FIXME: in history there two "=>" in each entry
-
 'use strict';
 
 const Util = require('./Util.js');
@@ -73,9 +71,7 @@ module.exports = (($) => {
               <div class="item-content swipeout-content">
                 <div class="item-inner">
                   <div class="item-title">
-                    ${historyEntry.dieSpec}
-                    ${_util.RESULT_SYMBOL}
-                    ${historyEntry.result}
+                    ${historyEntry.dieSpec}${historyEntry.result}
                   </div>
                 </div>
               </div>
